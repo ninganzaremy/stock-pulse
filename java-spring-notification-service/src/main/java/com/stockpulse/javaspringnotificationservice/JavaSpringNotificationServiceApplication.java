6b5aa5a829d2c09ae7bce49f2aed32fd9de6ea19
Remy Ninganza <remy@ninganzaremy.com>
2024-11-02 18:58:14 -0700

    initial gRPC microservices setup

---
+++ java-spring-notification-service/src/main/java/com/stockpulse/javaspringnotificationservice/JavaSpringNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stockpulse.javaspringnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ java-spring-notification-service/src/test/java/com/stockpulse/javaspringnotificationservice/JavaSpringNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpulse.javaspringnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
